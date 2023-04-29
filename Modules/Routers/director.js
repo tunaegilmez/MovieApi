0771fd812457959068038dfd8ca23e0105a01fb2
tunaegilmez <tunaegilmez07@gmail.com>
2023-04-29 16:49:32 +0300

    Get Directors and has movies

--- Modules/Routers/director.js
+++ Modules/Routers/director.js
@@ -3,6 +3,53 @@ const router = express.Router();
 
 import Director from "../Models/Director.js";
 
+router.get("/", (req, res) => {
+  const promise = Director.DirectorSchema.aggregate([
+    {
+      $lookup: {
+        from: "movies",
+        localField: "_id",
+        foreignField: "director_id",
+        as: "movies",
+      },
+    },
+    {
+      $unwind: {
+        path: "$movies",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $group: {
+        _id: {
+          _id: "$_id",
+          name: "$name",
+          surname: "$surname",
+          bio: "$bio",
+        },
+        movies: {
+          $push: "$movies",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: "$_id._id",
+        name: "$_id.name",
+        surname: "$_id.surname",
+        movies: "$movies",
+      },
+    },
+  ]);
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      req.json(err);
+    });
+});
+
 router.post("/", (req, res) => {
   const director = new Director.DirectorSchema(req.body);
   const promise = director.save();
