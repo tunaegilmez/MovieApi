4cc124d7d669d2d41908daf33083325bf9913d76
tunaegilmez <tunaegilmez07@gmail.com>
2023-04-29 18:12:25 +0300

    bcrypt pass

--- Modules/Routers/register.js
+++ Modules/Routers/register.js
@@ -1,24 +1,28 @@
 import express from "express";
 import User from "../Models/User.js";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/register", (req, res, next) => {
   const { username, password } = req.body;
-  const user = new User.UserSchema({
-    username,
-    password,
-  });
 
-  const promise = user.save();
-
-  promise
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json(err);
+  bcrypt.hash(password, 10).then(hash => {
+    const user = new User.UserSchema({
+      username,
+      password: hash,
     });
+
+    const promise = user.save();
+
+    promise
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 });
 
 export default router;
