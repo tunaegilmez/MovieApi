7a002d0802290ff39ca0c26a4d72b3c53b37a6eb
tunaegilmez <tunaegilmez07@gmail.com>
2023-04-29 17:00:38 +0300

    Get Director from ID

--- Modules/Routers/director.js
+++ Modules/Routers/director.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 const router = express.Router();
 
 import Director from "../Models/Director.js";
@@ -60,6 +61,58 @@ router.post("/", (req, res) => {
     })
     .catch(err => {
       res.json(err);
+    });
+});
+
+router.get("/:director_id", (req, res) => {
+  const promise = Director.DirectorSchema.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.params.director_id),
+      },
+    },
+    {
+      $lookup: {
+        from: "movies",
+        localField: "_id",
+        foreignField: "director_id",
+        as: "movies",
+      },
+    },
+    {
+      $unwind: {
+        path: "$movies",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $group: {
+        _id: {
+          _id: "$_id",
+          name: "$name",
+          surname: "$surname",
+          bio: "$bio",
+        },
+        movies: {
+          $push: "$movies",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: "$_id._id",
+        name: "$_id.name",
+        surname: "$_id.surname",
+        movies: "$movies",
+      },
+    },
+  ]);
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      req.json(err);
     });
 });
 export default router;
